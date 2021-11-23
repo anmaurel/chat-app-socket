211009ea7b293587431d2296b97c7a157b6823e1
Antoine Maurel <antoinemaurel6@gmail.com>
2021-11-23 10:20:45 +0100

    config: setup for cloud

--- back/server.js
+++ back/server.js
@@ -49,7 +49,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE,PATCH');
     next();
 });
 
